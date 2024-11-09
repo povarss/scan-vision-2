05a1a3b210176f5e8fdcae5431f494390234bfbb
Umidjon Hamidov <samirchik89@gmail.com>
2024-11-09 23:58:16 +0500

    add filter

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,16 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Patient;
 use App\Models\Reference;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
 class PatientController extends Controller
 {
     // Register a new user
-    public function list(Request $request, Reference $reference)
+    public function list(Request $request)
     {
         $model = Patient::query();
 
+        if (!empty($request->q)) {
+            $model->where(function (Builder $query) use ($request) {
+                $query->whereLike('full_name', '%' . $request->q . '%')
+                    ->orWhereLike('phone', '%' . $request->q . '%');
+            });
+        }
         return DataTables::eloquent($model)
             ->only(['id', 'full_name', 'phone', 'first_test', 'last_test'])
             ->addColumn('first_test', function (Patient $patient) {
