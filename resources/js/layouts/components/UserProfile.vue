81303dc5a4d7f3a82ba565af531e3b9b4d6e0eed
Umidjon Hamidov <samirchik89@gmail.com>
2024-11-09 21:31:14 +0500

    remove menu and add logout

--- resources/js/layouts/components/UserProfile.vue
+++ resources/js/layouts/components/UserProfile.vue
@@ -1,79 +1,85 @@
 <script setup>
-import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
+import { PerfectScrollbar } from "vue3-perfect-scrollbar";
 
-const router = useRouter()
-const ability = useAbility()
+const router = useRouter();
+const ability = useAbility();
 
 // TODO: Get type from backend
-const userData = useCookie('userData')
+const userData = useCookie("userData");
 
 const logout = async () => {
+  await $api("/logout", {
+    method: "POST",
+    onResponseError({ response }) {
+      errors.value = response._data.errors;
+    },
+  });
 
   // Remove "accessToken" from cookie
-  useCookie('accessToken').value = null
+  useCookie("accessToken").value = null;
 
   // Remove "userData" from cookie
-  userData.value = null
+  userData.value = null;
 
   // Redirect to login page
-  await router.push('/login')
+  await router.push("/login");
 
   // ℹ️ We had to remove abilities in then block because if we don't nav menu items mutation is visible while redirecting user to login page
 
   // Remove "userAbilities" from cookie
-  useCookie('userAbilityRules').value = null
+  useCookie("userAbilityRules").value = null;
 
   // Reset ability to initial ability
-  ability.update([])
-}
+  ability.update([]);
+};
 
 const userProfileList = [
-  { type: 'divider' },
-  {
-    type: 'navItem',
-    icon: 'tabler-user',
-    title: 'Profile',
-    to: {
-      name: 'apps-user-view-id',
-      params: { id: 21 },
-    },
-  },
-  {
-    type: 'navItem',
-    icon: 'tabler-settings',
-    title: 'Settings',
-    to: {
-      name: 'pages-account-settings-tab',
-      params: { tab: 'account' },
-    },
-  },
-  {
-    type: 'navItem',
-    icon: 'tabler-file-dollar',
-    title: 'Billing Plan',
-    to: {
-      name: 'pages-account-settings-tab',
-      params: { tab: 'billing-plans' },
-    },
-    badgeProps: {
-      color: 'error',
-      content: '4',
-    },
-  },
-  { type: 'divider' },
-  {
-    type: 'navItem',
-    icon: 'tabler-currency-dollar',
-    title: 'Pricing',
-    to: { name: 'pages-pricing' },
-  },
-  {
-    type: 'navItem',
-    icon: 'tabler-question-mark',
-    title: 'FAQ',
-    to: { name: 'pages-faq' },
-  },
-]
+  { type: "divider" },
+  // {
+  //   type: 'navItem',
+  //   icon: 'tabler-user',
+  //   title: 'Profile',
+  //   to: {
+  //     name: 'apps-user-view-id',
+  //     params: { id: 21 },
+  //   },
+  // },
+  // {
+  //   type: 'navItem',
+  //   icon: 'tabler-settings',
+  //   title: 'Settings',
+  //   to: {
+  //     name: 'pages-account-settings-tab',
+  //     params: { tab: 'account' },
+  //   },
+  // },
+  // {
+  //   type: 'navItem',
+  //   icon: 'tabler-file-dollar',
+  //   title: 'Billing Plan',
+  //   to: {
+  //     name: 'pages-account-settings-tab',
+  //     params: { tab: 'billing-plans' },
+  //   },
+  //   badgeProps: {
+  //     color: 'error',
+  //     content: '4',
+  //   },
+  // },
+  // { type: 'divider' },
+  // {
+  //   type: 'navItem',
+  //   icon: 'tabler-currency-dollar',
+  //   title: 'Pricing',
+  //   to: { name: 'pages-pricing' },
+  // },
+  // {
+  //   type: 'navItem',
+  //   icon: 'tabler-question-mark',
+  //   title: 'FAQ',
+  //   to: { name: 'pages-faq' },
+  // },
+];
 </script>
 
 <template>
@@ -92,22 +98,11 @@ const userProfileList = [
       :color="!(userData && userData.avatar) ? 'primary' : undefined"
       :variant="!(userData && userData.avatar) ? 'tonal' : undefined"
     >
-      <VImg
-        v-if="userData && userData.avatar"
-        :src="userData.avatar"
-      />
-      <VIcon
-        v-else
-        icon="tabler-user"
-      />
+      <VImg v-if="userData && userData.avatar" :src="userData.avatar" />
+      <VIcon v-else icon="tabler-user" />
 
       <!-- SECTION Menu -->
-      <VMenu
-        activator="parent"
-        width="240"
-        location="bottom end"
-        offset="12px"
-      >
+      <VMenu activator="parent" width="240" location="bottom end" offset="12px">
         <VList>
           <VListItem>
             <div class="d-flex gap-2 align-center">
@@ -121,17 +116,18 @@ const userProfileList = [
                   bordered
                 >
                   <VAvatar
-                    :color="!(userData && userData.avatar) ? 'primary' : undefined"
-                    :variant="!(userData && userData.avatar) ? 'tonal' : undefined"
+                    :color="
+                      !(userData && userData.avatar) ? 'primary' : undefined
+                    "
+                    :variant="
+                      !(userData && userData.avatar) ? 'tonal' : undefined
+                    "
                   >
                     <VImg
                       v-if="userData && userData.avatar"
                       :src="userData.avatar"
                     />
-                    <VIcon
-                      v-else
-                      icon="tabler-user"
-                    />
+                    <VIcon v-else icon="tabler-user" />
                   </VAvatar>
                 </VBadge>
               </VListItemAction>
@@ -140,47 +136,28 @@ const userProfileList = [
                 <h6 class="text-h6 font-weight-medium">
                   {{ userData.fullName || userData.username }}
                 </h6>
-                <VListItemSubtitle class="text-capitalize text-disabled">
+                <!-- <VListItemSubtitle class="text-capitalize text-disabled">
                   {{ userData.role }}
-                </VListItemSubtitle>
+                </VListItemSubtitle> -->
               </div>
             </div>
           </VListItem>
 
           <PerfectScrollbar :options="{ wheelPropagation: false }">
-            <template
-              v-for="item in userProfileList"
-              :key="item.title"
-            >
-              <VListItem
-                v-if="item.type === 'navItem'"
-                :to="item.to"
-              >
+            <template v-for="item in userProfileList" :key="item.title">
+              <VListItem v-if="item.type === 'navItem'" :to="item.to">
                 <template #prepend>
-                  <VIcon
-                    :icon="item.icon"
-                    size="22"
-                  />
+                  <VIcon :icon="item.icon" size="22" />
                 </template>
 
                 <VListItemTitle>{{ item.title }}</VListItemTitle>
 
-                <template
-                  v-if="item.badgeProps"
-                  #append
-                >
-                  <VBadge
-                    rounded="sm"
-                    class="me-3"
-                    v-bind="item.badgeProps"
-                  />
+                <template v-if="item.badgeProps" #append>
+                  <VBadge rounded="sm" class="me-3" v-bind="item.badgeProps" />
                 </template>
               </VListItem>
 
-              <VDivider
-                v-else
-                class="my-2"
-              />
+              <VDivider v-else class="my-2" />
             </template>
 
             <div class="px-4 py-2">
